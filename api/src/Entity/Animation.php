75539f53860a34c2e5d5272ea04e5ddb46b5a64d
Nicolas MYLLE <millt.nico@hotmail.fr>
2021-04-28 15:26:30 +0200

    Add tests, fixtures and enable field.

--- api/src/Entity/Animation.php
+++ api/src/Entity/Animation.php
@@ -2,14 +2,30 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
 use App\Repository\AnimationRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ApiResource()
  * @ORM\Entity(repositoryClass=AnimationRepository::class)
  */
+#[ApiResource(
+    collectionOperations: [
+    'get',
+    'post' => ['security' => "is_granted('ROLE_ADMIN')"],
+],
+    itemOperations: [
+    'get',
+    'put' => ['security' => "is_granted('ROLE_ADMIN')"],
+    'delete' => ['security' => "is_granted('ROLE_ADMIN')"],
+],
+    denormalizationContext: ['groups' => ['animation_write']],
+    normalizationContext: ['groups' => ['animation_read']],
+)]
+#[ApiFilter(BooleanFilter::class, properties: ['enabled'])]
 class Animation
 {
     /**
@@ -17,26 +33,29 @@ class Animation
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private int $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $name;
+    #[Groups(['animation_read', 'animation_write'])]
+    private string $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $lottieLink;
+    #[Groups(['animation_read', 'animation_write'])]
+    private string $lottieLink;
 
     /**
      * @ORM\Column(type="boolean")
      */
-    private $enabled;
+    #[Groups(['animation_read', 'animation_write'])]
+    private bool $enabled;
 
     public function __construct()
     {
-        if (!$this->enabled) {
+        if (!isset($this->enabled)) {
             $this->enabled = true;
         }
     }
--- api/tests/AnimationTest.php
+++ api/tests/AnimationTest.php
@@ -32,6 +32,67 @@ class AnimationTest extends CustomApiTestCase
         ]);
         $this->assertCount(30, $response->toArray()['hydra:member']);
         $this->assertMatchesResourceCollectionJsonSchema(Animation::class);
+
+        $item = $response->toArray()['hydra:member'][0];
+        $this->assertNotNull($item['name']);
+    }
+
+    public function testGetAllEnabledAnimations(): void
+    {
+        $client = self::createClientWithCredentials();
+
+        $response = $client->request('GET', '/animations', ['extra' => ['parameters' => ['enabled' => 'true']]]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/contexts/Animation',
+            '@id' => '/animations',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 49,
+            'hydra:view' => [
+                '@id' => '/animations?enabled=true&page=1',
+                '@type' => 'hydra:PartialCollectionView',
+                'hydra:first' => '/animations?enabled=true&page=1',
+                'hydra:next' => '/animations?enabled=true&page=2',
+                'hydra:last' => '/animations?enabled=true&page=2',
+            ],
+        ]);
+        $this->assertCount(30, $response->toArray()['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Animation::class);
+
+        $item = $response->toArray()['hydra:member'][0];
+        $this->assertNotNull($item['name']);
+        $this->assertTrue($item['enabled']);
+    }
+
+    public function testGetAllDisabledAnimations(): void
+    {
+        $client = self::createClientWithCredentials();
+
+        $response = $client->request('GET', '/animations', ['extra' => ['parameters' => ['enabled' => 'false']]]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/contexts/Animation',
+            '@id' => '/animations',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 51,
+            'hydra:view' => [
+                '@id' => '/animations?enabled=false&page=1',
+                '@type' => 'hydra:PartialCollectionView',
+                'hydra:first' => '/animations?enabled=false&page=1',
+                'hydra:next' => '/animations?enabled=false&page=2',
+                'hydra:last' => '/animations?enabled=false&page=2',
+            ],
+        ]);
+        $this->assertCount(30, $response->toArray()['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Animation::class);
+
+        $item = $response->toArray()['hydra:member'][0];
+        $this->assertNotNull($item['name']);
+        $this->assertFalse($item['enabled']);
     }
 
     public function testGetOneAnimation(): void
@@ -53,19 +114,26 @@ class AnimationTest extends CustomApiTestCase
     {
         $client = self::createClientWithCredentials();
 
-        $iri = $this->findIriBy(Animation::class, ['name' => 'Fireworks']);
-        $client->request('POST', $iri);
+        $client->request('POST', '/animations', [
+            'json' => [
+                'name' => 'superAnim',
+                'lottieLink' => 'https://lottiefiles.com/59444-ufo-error-page-404',
+                'enabled' => false,
+            ]
+        ]);
         $this->assertResponseStatusCodeSame(403);
     }
 
     public function testCreateAnimationAsAdmin(): void
     {
-        $client = self::createClientWithCredentials();
+        $client = self::createClientWithCredentials($this->getToken([
+            'email' => 'admin@example.com',
+            'password' => 'seCrEt',
+        ]));
 
-        $iri = $this->findIriBy(Animation::class, ['name' => 'Fireworks']);
-        $client->request('POST', $iri, [
+        $client->request('POST', '/animations', [
             'json' => [
-                'name' => 'superAnim',
+                'name' => 'super animation',
                 'lottieLink' => 'https://lottiefiles.com/59444-ufo-error-page-404',
                 'enabled' => false,
             ]
@@ -74,7 +142,7 @@ class AnimationTest extends CustomApiTestCase
         $this->assertMatchesResourceItemJsonSchema(Animation::class);
         $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
         $this->assertJsonContains([
-            'name' => 'superAnim',
+            'name' => 'super animation',
             'lottieLink' => 'https://lottiefiles.com/59444-ufo-error-page-404',
             'enabled' => false,
         ]);
@@ -85,15 +153,28 @@ class AnimationTest extends CustomApiTestCase
         $client = self::createClientWithCredentials();
 
         $iri = $this->findIriBy(Animation::class, ['name' => 'Fireworks']);
-        $client->request('PUT', $iri);
+        $client->request('PUT', $iri, [
+            'json' => ['enabled' => false,]
+        ]);
+        $this->assertResponseStatusCodeSame(403);
     }
 
     public function testUpdateAnimationAsAdmin(): void
     {
-        $client = self::createClientWithCredentials();
+        $client = self::createClientWithCredentials($this->getToken([
+            'email' => 'admin@example.com',
+            'password' => 'seCrEt',
+        ]));
 
         $iri = $this->findIriBy(Animation::class, ['name' => 'Fireworks']);
-        $client->request('PUT', $iri);
+        $client->request('PUT', $iri, [
+            'json' => ['enabled' => false,]
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            'name' => 'Fireworks',
+            'enabled' => false,
+        ]);
     }
 
     public function testDeleteAnimationAsUser(): void
@@ -102,13 +183,22 @@ class AnimationTest extends CustomApiTestCase
 
         $iri = $this->findIriBy(Animation::class, ['name' => 'Fireworks']);
         $client->request('DELETE', $iri);
+        $this->assertResponseStatusCodeSame(403);
     }
 
     public function testDeleteAnimationAsAdmin(): void
     {
-        $client = self::createClientWithCredentials();
+        $client = self::createClientWithCredentials($this->getToken([
+            'email' => 'admin@example.com',
+            'password' => 'seCrEt',
+        ]));
 
         $iri = $this->findIriBy(Animation::class, ['name' => 'Fireworks']);
         $client->request('DELETE', $iri);
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(204);
+        $this->assertNull(
+            static::$container->get('doctrine')->getRepository(Animation::class)->findOneBy(['name' => 'Fireworks'])
+        );
     }
 }
