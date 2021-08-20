b2c7b66b3846020909ae8b79c306ef60934a5a53
Nicolas MYLLE <millt.nico@hotmail.fr>
2021-08-20 15:53:13 +0200

    Fix entrypoint to deploy in production

--- pwa/config/entrypoint.ts
+++ pwa/config/entrypoint.ts
@@ -1,3 +1,3 @@
 import * as process from "process";
 
-export const ENTRYPOINT = typeof window === "undefined" ? process.env.NEXT_PUBLIC_API_ENTRYPOINT : window.origin;
+export const ENTRYPOINT = typeof window === "undefined" ? process.env.NEXT_PUBLIC_API_ENTRYPOINT : process.env.NEXT_PUBLIC_API_ENTRYPOINT !== 'http://caddy' ? process.env.NEXT_PUBLIC_API_ENTRYPOINT : window.origin;
