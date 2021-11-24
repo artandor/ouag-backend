dc872af3fef0efe810797eeab00482b4cdc10df8
Artandor <millt.nico@hotmail.fr>
2021-11-24 23:39:50 +0100

    Add coupon logic to front and back.

--- pwa/pages/gifts/[id]/summary.tsx
+++ pwa/pages/gifts/[id]/summary.tsx
@@ -5,10 +5,14 @@ import {useEffect, useState} from "react";
 import useTranslation from "next-translate/useTranslation";
 import {useRouter} from "next/router";
 import Button from "react-bootstrap/Button"
+import {Simulate} from "react-dom/test-utils";
 
 export default function SummaryPage() {
     const {t} = useTranslation('gifts');
     let [gift, setGift] = useState({})
+    const [promoCode, setPromoCode] = useState("");
+    const [promoCodeIsCorrect, setPromoCodeIsCorrect] = useState(null);
+    const [promotion, setPromotion] = useState(null);
     const router = useRouter()
 
     const unitPrice = 0.33;
@@ -21,8 +25,21 @@ export default function SummaryPage() {
             .catch(() => null);
     }, [router])
 
+    function verifyPromoCode() {
+        fetch('/stripe/coupon/validate', {
+            method: 'POST',
+            body: JSON.stringify({'code': promoCode})
+        }).then((data) => {
+            setPromotion(data)
+            setPromoCodeIsCorrect(true)
+        }).catch((err) => {
+            console.log('Could not find promotion code.');
+            setPromoCodeIsCorrect(false);
+        })
+    }
+
     function orderGiftRequest() {
-        fetch(router.asPath.replace('summary', 'order'), {method: 'PUT', body: JSON.stringify({})})
+        fetch(router.asPath.replace('summary', 'order'), {method: 'PUT', body: JSON.stringify({'code': promoCode})})
             .then((giftData) => {
                 console.log('Gift successfully published');
                 if (window) window.location.assign(giftData['checkoutUrl'])
@@ -78,8 +95,63 @@ export default function SummaryPage() {
                             <th>{t('recap.total')}</th>
                             <td>{gift['invites'] && gift['mediaAmount'] * unitPrice * gift['invites'].length} €</td>
                         </tr>
+                        {promotion && promotion['percent_off'] && (
+                            <>
+                                <tr>
+                                    <td scope="row" colSpan={2}/>
+                                    <th>{t('recap.reduction')} ({promotion['percent_off']} %)</th>
+                                    <td>- {(gift['mediaAmount'] * unitPrice * gift['invites'].length * promotion['percent_off'] / 100).toFixed(2)} €
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row" colSpan={2}/>
+                                    <th>{t('recap.newTotal')}</th>
+                                    <td>{gift['invites'] && (gift['mediaAmount'] * unitPrice * gift['invites'].length
+                                    - (gift['mediaAmount'] * unitPrice * gift['invites'].length * promotion['percent_off'] / 100)).toFixed(2)} €
+                                    </td>
+                                </tr>
+                            </>
+                        )
+                        }
+                        {promotion && promotion['amount_off'] && (
+                            <>
+                                <tr>
+                                    <td scope="row" colSpan={2}/>
+                                    <th>{t('recap.reduction')}</th>
+                                    <td>- {promotion['amount_off']} €
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td scope="row" colSpan={2}/>
+                                    <th>{t('recap.newTotal')}</th>
+                                    <td>{gift['invites'] && gift['mediaAmount'] * unitPrice * gift['invites'].length
+                                    - promotion['amount_off']} €
+                                    </td>
+                                </tr>
+                            </>
+                        )
+                        }
                         </tbody>
                     </table>
+                    <div className="d-flex justify-content-end">
+                        <div className="col-3 me-2">
+                            <input
+                                className={promoCodeIsCorrect === false ? "form-control is-invalid" :
+                                    promoCodeIsCorrect === true ? "form-control is-valid" : "form-control"}
+                                placeholder={t('recap.promotionnalCodeLabel')} value={promoCode}
+                                id="promoCode"
+                                onChange={(event) => setPromoCode(event.target.value)}/>
+                        </div>
+                        {!promotion &&
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary mb-3" disabled={promoCode.length <= 0}
+                                    onClick={verifyPromoCode}>
+                                {t('shared:submitButton')}
+                            </button>
+                        </div>
+                        }
+                    </div>
+
                     <p className="text-end">{t('recap.nonApplicableVat')}</p>
                 </div>
             </ContainerLayout>
